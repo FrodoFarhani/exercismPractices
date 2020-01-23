d31905985bec67636f751392da2da65ac99a1d30
so0oshiance <mohamad.farhany@gmail.com>
2020-01-23 16:47:46 +0330

    Bob more clean

--- 004-bob/bob-Clean.ts
+++ 004-bob/bob-Clean.ts
@@ -1,3 +1,5 @@
+import { promises } from 'dns';
+
 enum ANSWERS {
   sayingAnything = 'Fine. Be that way!',
   askQuestion = 'Sure.',
@@ -11,72 +13,70 @@ enum LASTCHARACTER {
 }
 
 class Bob {
-  constructor() {}
-  private inputString: string = '';
-  private lastChar: string = '';
-  private isUpperCase: boolean = false;
+  private inputString = '';
+  private lastChar = '';
+  private isUpperCase = false;
+  private answer = ANSWERS.default;
 
-  private async clearAllSpaces() {
+  private async clearAllSpaces(): Promise<void> {
     const FIND_SPACES = new RegExp(/\s/g);
     this.inputString = this.inputString.replace(FIND_SPACES, '');
   }
-  private async getLastCharacter() {
+  private async getLastCharacter(): Promise<void> {
     this.lastChar = this.inputString.slice(-1);
   }
-  private async getOnlyAlphabets() {
+  private async getOnlyAlphabets(): Promise<void> {
     const FIND_ALL_ALPHABETS = new RegExp(/[a-z]+/gi);
     this.inputString = (
       this.inputString.match(FIND_ALL_ALPHABETS) || ''
     ).toString();
   }
-  private async checkAlphabetState() {
+  private async checkAlphabetState(): Promise<void> {
     const CHECK_ALL_UPPERCASE = new RegExp(/\b[A-Z0-9]+\b/);
     this.isUpperCase = this.inputString.match(CHECK_ALL_UPPERCASE)
       ? true
       : false;
   }
-  private async sayAnything() {
+  private async sayAnything(): Promise<boolean> {
     const isSayAnything = !this.inputString;
+    isSayAnything ? (this.answer = ANSWERS.sayingAnything) : this.answer;
     return isSayAnything;
   }
-  private async askQuestion() {
+  private async askQuestion(): Promise<void> {
     const isQuestion =
       (this.inputString &&
         LASTCHARACTER.QuestionMark == this.lastChar &&
         !this.isUpperCase) ||
       (!this.inputString && LASTCHARACTER.QuestionMark == this.lastChar);
-    return isQuestion;
+    isQuestion ? (this.answer = ANSWERS.askQuestion) : this.answer;
   }
-  private async yellAtHim() {
+  private async yellAtHim(): Promise<void> {
     const isYell =
       (this.isUpperCase && LASTCHARACTER.ExclamationMark == this.lastChar) ||
       (this.isUpperCase && LASTCHARACTER.QuestionMark != this.lastChar);
-    return isYell;
+    isYell ? (this.answer = ANSWERS.yellAtHim) : this.answer;
   }
-  private async yellAndQuestion() {
+  private async yellAndQuestion(): Promise<void> {
     const isYellAndQuestion =
       this.isUpperCase && LASTCHARACTER.QuestionMark == this.lastChar;
-    return isYellAndQuestion;
+    isYellAndQuestion ? (this.answer = ANSWERS.yellAndQuestion) : this.answer;
   }
-  async hey(inputString: string) {
+  async hey(inputString: string): Promise<string> {
     this.inputString = inputString;
 
     await this.clearAllSpaces();
     await this.getLastCharacter();
 
-    if (await this.sayAnything()) return ANSWERS.sayingAnything;
+    if (await this.sayAnything()) return this.answer;
 
     await this.getOnlyAlphabets();
-
     await this.checkAlphabetState();
 
-    if (await this.askQuestion()) return ANSWERS.askQuestion;
+    await this.askQuestion();
+    await this.yellAtHim();
+    await this.yellAndQuestion();
 
-    if (await this.yellAtHim()) return ANSWERS.yellAtHim;
-
-    if (await this.yellAndQuestion()) return ANSWERS.yellAndQuestion;
-
-    return ANSWERS.default;
+    return this.answer;
   }
 }
 
