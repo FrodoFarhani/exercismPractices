61902c514a09f164ade65a64ad7388a84d38f2bc
so0oshiance <mohamad.farhany@gmail.com>
2020-04-07 15:27:47 +0430

    atbash cipher

--- 009-atbash-cipher/atbash-cipher.ts
+++ 009-atbash-cipher/atbash-cipher.ts
@@ -3,23 +3,22 @@ export default class AtbashCipher {
   constructor() {
     this.plain = "abcdefghijklmnopqrstuvwxyz";
   }
-  encode(input: string): string {
-    input = this.clearText(input);
-
-    const result = this.cipher(input)
+  encode = (input: string): string =>
+    this.cipher(input)
       .match(/.{0,5}/g)!
       .join(" ")
       .trim();
-    return result;
-  }
-  decode(input: string): string {
-    input = this.clearText(input);
-
-    const result = this.cipher(input).trim();
-    return result;
-  }
-
+  decode = (input: string): string => this.cipher(input).trim();
+  private clearText = (input: string): string =>
+    input
+      .split(",")
+      .join("")
+      .split(".")
+      .join("")
+      .toLowerCase()
+      .trim();
   private cipher(input: string): string {
+    input = this.clearText(input);
     const result = Array.from(input).map(item => {
       const inverseIndex = Math.abs(
         this.plain.indexOf(item) - this.plain.length + 1
@@ -27,16 +26,5 @@ export default class AtbashCipher {
       return !parseInt(item) ? this.plain[inverseIndex] : item;
     });
     return result.join("");
-  }
-
-  private clearText(input: string): string {
-    return input
-      .split(" ")
-      .join("")
-      .split(",")
-      .join("")
-      .split(".")
-      .join("")
-      .toLowerCase();
   }
 }
